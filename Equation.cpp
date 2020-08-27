c2866f8db57c36c205a6c920f5d2c30535cc0a2b
ochenudivlen <70331127+ochenudivlen@users.noreply.github.com>
2020-08-27 19:06:10 +0300

    Add files via upload

---
+++ Equation.cpp
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <math.h>
+
+int const INFINITELY = -1;
+
+//-----------------------------------------------------------------------------
+
+int Linear_equation (double coeff_a, double coeff_b, double* result)   // equation ax+b=0
+{
+   if (coeff_a == 0)
+   {
+      if (coeff_b == 0)
+      {
+         return INFINITELY;
+      }
+
+      return 0;   // b != 0
+   }
+
+   *result = - coeff_b / coeff_a;   // a != 0
+   return 1;
+}
+
+//-----------------------------------------------------------------------------
+
+int Quadratic_equation (double coeff_a, double coeff_b, double coeff_c, double* result1, double* result2)   // equation ax^2+bx+c=0
+{
+   double diskr = coeff_b * coeff_b - 4.0 * coeff_a * coeff_c;
+
+   if (diskr < 0)
+   {
+      return 0;
+   }
+   if (diskr == 0)
+   {
+      *result1 = *result2 = -coeff_b/(2*coeff_a);
+      return 1;
+   }
+      *result1 = (-coeff_b + sqrt (diskr))/(2*coeff_a);
+      *result2 = (-coeff_b - sqrt (diskr))/(2*coeff_a);
+      return 2;
+}
+
+//-----------------------------------------------------------------------------
+
+int main()
+{
+   printf ("# Solution of a quadratic equation\n"
+           "# 27.08.2020\n\n");
+   printf ("#Enter coefficients 1, 2, 3: ");
+
+   double coeff1=0, coeff2=0, coeff3=0;
+   scanf ("%lg %lg %lg", &coeff1, &coeff2, &coeff3);
+
+   double result1=0, result2=0;
+
+   int Solutions;
+   if (coeff1 == 0)
+   {
+      Solutions = Linear_equation (coeff2, coeff3, &result1);
+   }
+   else
+   {
+      Solutions = Quadratic_equation (coeff1, coeff2, coeff3, &result1, &result2);
+   }
+
+   switch (Solutions)
+   {
+      case 0:
+
+              printf ("No roots\n");
+              break;
+
+      case 1:
+
+              printf ("root: %lg\n", result1);
+              break;
+
+      case 2:
+
+              printf ("root 1: %lg, root 2: %lg\n", result1, result2);
+              break;
+
+      case INFINITELY:
+
+                       printf ("root - any number");
+                       break;
+
+      default:
+
+               printf ("main(): data entry error");
+               return 1;
+               break;
+   }
+
+   return 0;
+}
