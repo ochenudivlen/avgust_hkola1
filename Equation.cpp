dfa2543a171490ba69e805844d9563b50e358b8b
ochenudivlen <70331127+ochenudivlen@users.noreply.github.com>
2020-08-28 21:37:58 +0300

    Add files via upload

--- Equation.cpp
+++ Equation.cpp
@@ -1,101 +1,47 @@
 #include <stdio.h>
 #include <math.h>
-
-int const INFINITELY = -1;
-double const ERROR = 0.0000001;
-
+#include <assert.h>
 
 //-----------------------------------------------------------------------------
 
-int CompareZero (double Number)
-{
-   if (Number < -ERROR)
-   {
-      return -1;   //---Number < 0
-   }
-
-   if (fabs(Number) < ERROR)
-   {
-      return 0;   //---Number == 0
-   }
-
-   return 1;   //---Number>0
-}
+int const INFINITELY            = -1;
+int const NOROOTS               = -2;
+int const BEFORE_CHECK          = NAN;
+double const ERROR              = 0.0000001;
+double const BEFORE_DETERMINING = NAN;
 
 //-----------------------------------------------------------------------------
 
-int Linear_equation (double coeff_a, double coeff_b, double* result)   //---equation ax + b = 0
-{
-   if (CompareZero (coeff_a) == 0 && CompareZero (coeff_b) == 0)
-   {
-      return INFINITELY;
-   }
+int CompareZero (double number);
 
-   //---Now not INFINITELY
+int Linear_equation (double coeff_a, double coeff_b, double* result);
 
-   if (CompareZero (coeff_a) == 0)
-   {
-      return 0;   //---b != 0
-   }
+int Quadratic_equation (double coeff_a, double coeff_b, double coeff_c, double* result1, double* result2);
 
-   *result = -coeff_b / coeff_a;   //---a != 0
-   return 1;
-}
+void Solve_equation (double coeff_a, double coeff_b, double coeff_c, int* solutions, double* result1, double* result2);
 
-//-----------------------------------------------------------------------------
-
-int Quadratic_equation (double coeff_a, double coeff_b, double coeff_c, double* result1, double* result2)   //---equation ax^2 + bx + c = 0
-{
-   double diskr = coeff_b * coeff_b - 4 * coeff_a * coeff_c;
-
-   if (CompareZero (diskr) == -1)
-   {
-      return 0;
-   }
-
-   if (CompareZero (diskr) == 0)
-   {
-      *result1 = *result2 = -coeff_b / (2 * coeff_a);
-      return 1;
-   }
-
-      *result1 = (-coeff_b + sqrt (diskr)) / (2 * coeff_a);
-      *result2 = (-coeff_b - sqrt (diskr)) / (2 * coeff_a);
-      return 2;
-}
-
-//-----------------------------------------------------------------------------
-
-void Solve_equation (double coeff1, double coeff2, double coeff3, int* Solutions, double* result1, double* result2)
-{
-   if (CompareZero (coeff1) == 0)
-   {
-      *Solutions = Linear_equation (coeff2, coeff3, result1);
-   }
-   else
-   {
-      *Solutions = Quadratic_equation (coeff1, coeff2, coeff3, result1, result2);
-   }
-}
+void Checking_coeff (double* coeff_a, double* coeff_b, double* coeff_c);
 
 //-----------------------------------------------------------------------------
 
 int main()
-{
+   {
    printf ("# Solution of a quadratic equation\n"
            "# 27.08.2020\n\n");
-   printf ("# Enter coefficients 1, 2, 3: ");
+   printf ("# Enter coefficients a, b, c: ");
 
-   double coeff1 = 0, coeff2 = 0, coeff3 = 0;
-   scanf ("%lg %lg %lg", &coeff1, &coeff2, &coeff3);
+   double coeff_a = BEFORE_DETERMINING, coeff_b = BEFORE_DETERMINING, coeff_c = BEFORE_DETERMINING;
+   Checking_coeff (&coeff_a, &coeff_b, &coeff_c);
+//   scanf ("%lg %lg %lg", &coeff_a, &coeff_b, &coeff_c);
+//   printf ("%lg %lg %lg", coeff_a, coeff_b, coeff_c);
 
-   double result1 = 0, result2 = 0;
+   double result1 = BEFORE_DETERMINING, result2 = BEFORE_DETERMINING;
 
-   int Solutions = 0;
-   Solve_equation (coeff1, coeff2, coeff3, &Solutions, &result1, &result2);
+   int solutions = NOROOTS;
+   Solve_equation (coeff_a, coeff_b, coeff_c, &solutions, &result1, &result2);
 
-   switch (Solutions)
-   {
+   switch (solutions)
+      {
       case 0:
               printf ("No roots\n");
               break;
@@ -116,7 +62,105 @@ int main()
                printf ("main(): data entry error");
                return 1;
                break;
-   }
+      }
 
    return 0;
-}
+   }
+
+//-----------------------------------------------------------------------------
+
+int CompareZero (double number)
+   {
+   if (number < -ERROR)
+      {
+      return -1;   //---number < 0
+      }
+
+   if (fabs(number) < ERROR)
+      {
+      return 0;   //---number == 0
+      }
+
+   return 1;   //---number>0
+   }
+
+//-----------------------------------------------------------------------------
+
+int Linear_equation (double coeff_a, double coeff_b, double* result)   //---equation ax + b = 0
+   {
+   if (CompareZero (coeff_a) == 0 && CompareZero (coeff_b) == 0)
+      {
+      return INFINITELY;
+      }
+
+   //---Now not INFINITELY
+
+   if (CompareZero (coeff_a) == 0)
+      {
+      return 0;   //---b != 0
+      }
+
+   *result = -coeff_b / coeff_a;   //---a != 0
+   return 1;
+   }
+
+//-----------------------------------------------------------------------------
+
+int Quadratic_equation (double coeff_a, double coeff_b, double coeff_c, double* result1, double* result2)   //---equation ax^2 + bx + c = 0
+   {
+   double diskr = coeff_b*coeff_b - 4*coeff_a*coeff_c;
+
+   if (CompareZero (diskr) == -1)
+      {
+      return 0;
+      }
+
+   if (CompareZero (diskr) == 0)
+      {
+      *result1 = *result2 = -coeff_b / (2*coeff_a);
+      return 1;
+      }
+
+      *result1 = (-coeff_b + sqrt (diskr)) / (2 * coeff_a);
+      *result2 = (-coeff_b - sqrt (diskr)) / (2 * coeff_a);
+      return 2;
+    }
+
+//-----------------------------------------------------------------------------
+
+void Solve_equation (double coeff_a, double coeff_b, double coeff_c, int* solutions, double* result1, double* result2)
+   {
+   assert (isfinite (coeff_a));
+   assert (isfinite (coeff_b));
+   assert (isfinite (coeff_c));
+   assert (coeff_a != NULL);
+   assert (coeff_b != NULL);
+   assert (coeff_c != NULL);
+
+   if (CompareZero (coeff_a) == 0)
+      {
+      *solutions = Linear_equation (coeff_b, coeff_c, result1);
+      }
+   else
+      {
+      *solutions = Quadratic_equation (coeff_a, coeff_b, coeff_c, result1, result2);
+      }
+   }
+
+//-----------------------------------------------------------------------------
+
+void Checking_coeff (double* coeff_a, double* coeff_b, double* coeff_c)
+   {
+   while (true)
+      {
+      if (scanf ("%lg %lg %lg/n", coeff_a, coeff_b, coeff_c) != 3)
+         {
+         printf ("Incorrect values. Re-enter:\n");
+         fflush (stdin);
+         }
+      else
+         {
+         return;
+         }
+      }
+   }
